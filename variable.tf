e6630291f77c6bfe8756f59f77dc630819a85657
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-04-20 22:11:33 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,14 @@
+variable "subscriptionId" {
+}
+
+variable "clientId" {
+}
+
+variable "clientSecret" {
+}
+
+variable "tenantId" {
+}
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the PostgreSQL Server. Changing this forces a new resource to be created."
   type        = string
